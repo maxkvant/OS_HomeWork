543fcda54386041c21341ab72ebccec9167e913d
Maksim Vinnichenko <max.kvant@gmail.com>
2016-10-01 16:13:03 +0300

    fixed backtrace

--- src/backtrace2.c
+++ src/backtrace2.c
@@ -1,13 +1,19 @@
 #include<lib.h>
+#include <stdarg.h>
+extern void* start64;
 
 void backtraceF(uint64_t* frame) { //frame: %rbp
     printf("backtrace {\n");
-    while(1) {
+    int max_stack_size = 1024;
+    
+    char* start64p = (char*)&start64;
+    
+    for (int i = 0; i < max_stack_size; i++) {
         printf("frame: %llx | returns_to (instruction): %llx\n", frame, *(frame+1));
-        if (*(frame + 1) <= 0x100695) {
+        frame = (uint64_t*)*frame;
+        if ((char*)*(frame + 1) == start64p + 0x22) {
             break;
         }
-        frame = (uint64_t*)*frame;
     } 
     printf("}\n");
 } 
